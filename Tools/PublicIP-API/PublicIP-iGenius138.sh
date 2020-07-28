100ae6230dfe525e60c9f3563f115add444543ad
Blue3 <hoanganh.do.fx@gmail.com>
2020-07-28 23:45:54 +0700

    Update PublicIP example Tool

--- Tools/PublicIP-API/PublicIP-iGenius138.sh
+++
@@ -1,3 +0,0 @@
-NewIP=`curl https://api.ipify.org`
-echo "{\"PublicIP\": \"$NewIP\"}" > PublicIPsh.txt
-curl -i -H "Content-Type: application/json" -X PUT -d "@/root/iGenius138/API/PublicIPsh.txt" http://www.hipt.vn:3500/api/devices/3
