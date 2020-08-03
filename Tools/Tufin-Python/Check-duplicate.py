c92826779aeae5da2bb1a27f94e3dc95c9c17329
Blue3 <hoanganh.do.fx@gmail.com>
2020-08-04 02:07:24 +0700

    Brand New Updated
    Brand New Updated

--- Tools/Tufin-Python/Check-duplicate.py
+++
@@ -1,8 +0,0 @@
-import os
-for f in os.listdir('./Affected/'):
-    if f.startswith("Device"):
-        for file in os.listdir('./'):
-            if file.startswith("Device"):
-                if file == f:
-                    os.remove(file)
-                    print(file+" has been removed!")
